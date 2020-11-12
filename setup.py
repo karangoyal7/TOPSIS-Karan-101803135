17a4913d7ab911cf22abb98ddb6ea127859398f7
karangoyal7 <74345717+karangoyal7@users.noreply.github.com>
2020-11-12 18:19:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Karan Goyal',                   # Type in your name
   author_email = 'karandps7@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/karangoyal7',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/karangoyal7/TOPSIS-Karan-101803135/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['TOPSIS', '101803135', 'Karan Goyal'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
